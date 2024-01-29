0e148ad51dd62bf2ec0f5d70aede04f40730dbf7
Théo Teenster <thewro11@gmail.com>
2024-01-30 00:27:32 +0700

    style pages and add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ChangePasswordPageComponent } from './change-password-page/change-password-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     ChangePasswordPageComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./src/**/*.{html,ts}",
+  "./node_modules/flowbite/**/*.js"
 ];
 export const theme = {
   extend: {},
 };
 export const plugins = [
   require('@tailwindcss/forms'),
+  require('flowbite/plugin'),
 ];
