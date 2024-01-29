4215b6cdf3c3563a9ce027f2f9e40705ca6c5d7d
Théo Teenster <thewro11@gmail.com>
2024-01-30 01:14:35 +0700

    add create account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ChangePasswordPageComponent } from './change-password-page/change-password-page.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,11 @@ const routes: Routes = [
   {
     path: "password/change",
     component: ChangePasswordPageComponent,
-  }
+  },
+  {
+    path: "account/create",
+    component: CreateAccountComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ChangePasswordPageComponent } from './change-password-page/change-password-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
     LoginPageComponent,
     ChangePasswordPageComponent,
     FooterComponent,
+    CreateAccountComponent,
   ],
   imports: [
     BrowserModule,
